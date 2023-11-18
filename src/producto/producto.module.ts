52311961af5556782f506991c83a1066826a1e63
Luis Correa <luis_correa.tekton@skyairline.com>
2023-11-18 18:08:12 -0500

    build controller

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductoService } from './producto.service';
 import { ProductoEntity } from './producto.entity';
+import { ProductoController } from './producto.controller';
 
 @Module({
     providers: [ProductoService],
-    imports: [TypeOrmModule.forFeature([ProductoEntity])]
+    imports: [TypeOrmModule.forFeature([ProductoEntity])],
+    controllers: [ProductoController]
 })
 export class ProductoModule {}
